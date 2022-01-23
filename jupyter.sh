d7ff71a51d79ff831558b9d79ecbdee9c7a12066
Marshall Asch <maasch@rogers.com>
2022-01-23 18:22:45 -0500

    copy the contents of the builtin folder not the actual folder

--- jupyter.sh
+++ jupyter.sh
@@ -3,6 +3,6 @@
 mkdir -p /course/jupyter/notebooks/builtin
 
 # add a symbolic link to the ones in the mounted volume
-cp -n -r -u -v /jupyter/builtin /course/jupyter/notebooks/builtin
+cp -n -r -u -v /jupyter/builtin/* /course/jupyter/notebooks/builtin/
 
 jupyter lab --ip=0.0.0.0
