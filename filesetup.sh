96528670e9d9ad5cca7b52ddaabff3c337e1c873
Marshall Asch <maasch@rogers.com>
2022-03-26 08:41:08 -0400

    fixed spelling mistake in lectures path

--- filesetup.sh
+++ filesetup.sh
@@ -5,6 +5,6 @@
 # Copy the builtin content to the mounted volume
 cp -n -r -u -v /builtin/jupyter/. /course/jupyter/notebooks/tutorials/
 cp -n -r -u -v /builtin/coursework/. /course/coursework/
-cp -n -r -u -v /builtin/lectures/. /course/lecturess/
+cp -n -r -u -v /builtin/lectures/. /course/lectures/
 cp -n -r -u -v /builtin/practiceProblems/. /course/practiceProblems/
 
